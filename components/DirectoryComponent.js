5ba27462d9906856dc0775b76a0c4de0ab773863
Peter Phan <peter_phan05@yahoo.com>
2021-03-02 03:26:46 -0800

    Components Part 2

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -3,12 +3,13 @@ import { FlatList } from "react-native";
 import { ListItem } from "react-native-elements";
 
 function Directory(props) {
-    
+
     const renderDirectoryItem = ({ item }) => {
         return (
             <ListItem
                 title={item.name}
                 subtitle={item.description}
+                onPress={() => props.onPress(item.id)}
                 leftAvatar={{ source: require("./images/react-lake.jpg") }}
             />
         );
