c6b9fefc23f700c74b8dbd2216789e6c6d6ed82e
Peter Phan <peter_phan05@yahoo.com>
2021-03-03 01:57:02 -0800

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,11 @@
-import React, { Component } from "react";
-import Directory from "./DirectoryComponent";
-import CampsiteInfo from "./CampsiteInfoComponent";
-import { View, Platform } from "react-native";
-import { createStackNavigator } from "react-navigation-stack";
-import { createAppContainer } from "react-navigation";
+import React, { Component } from 'react';
+import Home from './HomeComponent';
+import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
+import { View, Platform } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createAppContainer } from 'react-navigation';
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -11,20 +13,47 @@ const DirectoryNavigator = createStackNavigator(
         CampsiteInfo: { screen: CampsiteInfo },
     },
     {
-        initialRouteName: "Directory",
+        initialRouteName: 'Directory',
         defaultNavigationOptions: {
             headerStyle: {
-                backgroundColor: "#5637DD",
+                backgroundColor: '#5637DD',
             },
-            headerTintColor: "#fff",
+            headerTintColor: '#fff',
             headerTitleStyle: {
-                color: "#fff",
+                color: '#fff',
             },
         },
     }
 );
 
-const AppNavigator = createAppContainer(DirectoryNavigator);
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home },
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff',
+            },
+        },
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Directory: { screen: DirectoryNavigator },
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF',
+    }
+);
+
+const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
     render() {
@@ -33,7 +62,7 @@ class Main extends Component {
                 style={{
                     flex: 1,
                     paddingTop:
-                        Platform.OS === "ios"
+                        Platform.OS === 'ios'
                             ? 0
                             : Expo.Constants.statusBarHeight,
                 }}>
